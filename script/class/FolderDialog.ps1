b81d385f24871a4673aa42dbd0b0b559d67d9caa
takumi-t4096 <takumi_t@outlook.com>
2023-05-28 00:06:21 +0900

    add: FolderDialog.ps1, MsgBox.ps1ファイルを追加しました
    FolderDialog：フォルダダイアログ処理をまとめたクラスです
    MsgBox：ワーニングメッセージ処理をまとめたクラスです
    上記２つのクラスは別リポジトリ（folder-all-zipexpand）から流用しました

---
+++ script/class/FolderDialog.ps1
@@ -0,0 +1,34 @@
+﻿# FolderDialog Class
+
+# name space
+[void][System.Reflection.Assembly]::LoadWithPartialName("system.windows.forms")
+
+class FolderDialog {
+
+    [object] $dialog
+    [string] $selectDirpath
+
+    # コンストラクタ
+    FolderDialog() {
+        $this.dialog = New-Object System.Windows.Forms.FolderBrowserDialog
+        $this.dialog.Description = "フォルダ選択"
+        $this.selectDirpath = ""
+    }
+
+    # フォルダダイアログ実行
+    [boolean] showDialog() {
+        [boolean] $result = $false
+        
+        if($this.dialog.ShowDialog() -eq "ok"){
+            $result = $true
+            $this.selectDirpath = $this.dialog.SelectedPath
+        }
+
+        return $result
+    }
+
+    # フォルダダイアログで選択したフォルダまでのパスを取得
+    [string] getSelectDirPath() {
+        return $this.selectDirpath
+    }
+}
---
+++ script/class/MsgBox.ps1
@@ -0,0 +1,33 @@
+﻿# MsgBox Class
+
+# アセンブリのロード
+Add-Type -AssemblyName System.Windows.Forms
+
+class MsgBox {
+    [string] $msgStr = ""
+    [string] $msgTitle = ""
+    [string] $msgBtn = ""
+    [string] $msgIcon = ""
+    [string] $msgBtnPosition = ""
+    
+    # コンストラクタ
+    MsgBox() {}
+
+    # ワーニング
+    warningMsg([string] $msgStr) {
+        $this.msgStr = $msgStr
+        $this.msgTitle = "警告メッセージ"
+        $this.msgBtn = "OK"
+        $this.msgIcon = "Exclamation"
+        $this.msgBtnPosition = "button3"
+
+        [System.Windows.Forms.MessageBox]::Show(
+            $this.msgStr,
+            $this.msgTitle, 
+            $this.msgBtn,
+            $this.msgIcon,
+            $this.msgBtnPosition
+        )
+    }
+
+}
