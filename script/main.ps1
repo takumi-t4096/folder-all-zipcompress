8b86dfef58650ed1e4273decb9a89e36d6c0cf3b
takumi-t4096 <takumi_t@outlook.com>
2023-05-28 01:25:06 +0900

    update: main.ps1
    メイン処理を記述しました
    Windows PowerShell IDEから実際に動作することを確認しました

--- script/main.ps1
+++ script/main.ps1
@@ -1,7 +1,30 @@
 ﻿# include Class
-
+. $PSScriptRoot"\class\FolderDialog.ps1"
+. $PSScriptRoot"\class\MsgBox.ps1"
 
 
 function main() {
+    [object] $fd = New-Object FolderDialog
+    [object] $mb = New-Object MsgBox
+
+    if( !($fd.showDialog()) ) {
+        $mb.warningMsg("フォルダが選択されませんでした`r`n処理を終了しました")
+        exit
+    }
+
+    [array] $folders = Get-ChildItem -Path $fd.getSelectDirPath() -Directory -Name
+    
+    if($folders.Count -eq 0) {
+        $mb.warningMsg("フォルダ内に「.zip」ファイルが存在しません`r`n処理を終了しました")
+        exit
+    }
+
+    foreach($folder in $folders) {
+        [string] $targetPath = Join-Path $fd.getSelectDirPath() $folder
+        [string] $outputPath = $targetPath + ".zip"
+        Compress-Archive -Path $targetPath -DestinationPath $outputPath -Force
+    }
+
+    Write-Host "処理完了"
 }
 main
